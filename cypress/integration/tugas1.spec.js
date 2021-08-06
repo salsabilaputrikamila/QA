6e2f91f52421e54b027c9ab3fd6e2fcea67bfdbc
salsabilaputrikamila <60762348+salsabilaputrikamila@users.noreply.github.com>
2021-08-06 22:14:20 +0700

    commit

--- cypress/integration/tugas1.spec.js
+++ cypress/integration/tugas1.spec.js
@@ -3,6 +3,7 @@
 describe('User menuju ke halaman login dengan memasukkan url siakad', () => {
     it('Cek halaman berhasil diakses tidak', () => {
         cy.visit("http://siakad.polinema.ac.id/")
+        cy.url().should('eq', 'http://siakad.polinema.ac.id/')
     });
 
     it('Tes User masuk dengan Username yang valid dan Password tidak valid', () => {
@@ -10,6 +11,7 @@ describe('User menuju ke halaman login dengan memasukkan url siakad', () => {
         cy.get('#username').type('1831710184')
         cy.get('#password').type('salsa')
         cy.get('#form_login > div.form-actions > button').click()
+        cy.contains('Username / Password Salah')
     });
 
     it('Tes User tidak dapat login dengan username yang valid dan sandi yang tidak valid', () => {
@@ -17,6 +19,7 @@ describe('User menuju ke halaman login dengan memasukkan url siakad', () => {
         cy.get('#username').type('salsa')
         cy.get('#password').type('salsa')
         cy.get('#form_login > div.form-actions > button').click()
+        cy.contains('Username / Password Salah')
     });
 
     it('Tes User masuk dengan Username valid dan Password valid', () => {
@@ -33,17 +36,18 @@ describe('User menuju ke halaman login dengan memasukkan url siakad', () => {
         cy.get('[type="checkbox"]').click()
     });
 
-    // it('Tes User klik lupa Password', () => {
-    //     cy.visit("http://siakad.polinema.ac.id/")
-    //     cy.contains('Lupa Password?')
-    //     cy.get('#forget-password').click()
-    // });
+    it('Tes User klik lupa Password', () => {
+        cy.visit("http://siakad.polinema.ac.id/","")
+        cy.contains('Lupa Password?')
+        cy.get('#forget-password').click()
+    });
     
     it('Tes User masuk dengan mengkosongkan Username dan Password', () => {
         cy.visit("http://siakad.polinema.ac.id/")
         cy.get('#username').clear()
         cy.get('#password').clear()
         cy.get('#form_login > div.form-actions > button').click()
+        cy.contains('Username harus diisi')
     });
 
     it('Tes User masuk dengan mengkosongkan Username namun Password diisi', () => {
@@ -51,6 +55,8 @@ describe('User menuju ke halaman login dengan memasukkan url siakad', () => {
         cy.get('#username').clear()
         cy.get('#password').type('salsa')
         cy.get('#form_login > div.form-actions > button').click()
+        cy.contains('Username harus diisi')
+
     });
 
     it('Tes User masuk dengan mengisikan Username namun Password dikosongkan', () => {
@@ -58,6 +64,7 @@ describe('User menuju ke halaman login dengan memasukkan url siakad', () => {
         cy.get('#username').type('1831710184')
         cy.get('#password').clear()
         cy.get('#form_login > div.form-actions > button').click()
+        cy.contains('Password harus diisi')
     });
     
 });
